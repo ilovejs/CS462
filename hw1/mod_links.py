ae94cf210e20593c1f3d1d61fc05cff3bee31829
Doug von Kohorn <dougvk@gmail.com>
2010-09-24 02:55:03 -0400

    couple mods of mod_links. decided to use node with most outlinks for start of BFS

--- hw1/mod_links.py
+++ hw1/mod_links.py
@@ -2,7 +2,7 @@
 import re, sys
 
 grouper = re.compile(r'(\d+): (.*)$')
-outFile = open('wiki-links.txt', 'w')
+outFile = open('/media/SW_Preload/wiki-links.txt', 'w')
 
 def mod(line):
     groups = grouper.match(line)
@@ -12,16 +12,15 @@ def mod(line):
     linkList = ';'.join(links)
 
     string = ""
-    if(node != 5250701):
+    if(node != 3015959):
         string = str(node)+"|"+str(sys.maxint)+"|"+linkList+"\n"
-    if(node == 5250701):
-        string = "5250701|0|"+linkList+"\n"
+    if(node == 3015959):
+        string = "3015959|0|"+linkList+"\n"
 
     outFile.write(string)
     
 def main():
-    lines = open(sys.argv[1], 'r').read().splitlines()
-    print sys.argv[1]
+    lines = open(sys.argv[1], 'r').xreadlines()
     for line in lines:
         mod(line)
     outFile.close()

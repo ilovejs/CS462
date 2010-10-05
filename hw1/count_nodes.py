ae03c239bf1f6bd74eb79b98790ac0bfaf108301
Doug von Kohorn <dougvk@gmail.com>
2010-10-05 02:21:10 -0400

    hw1 finished

--- hw1/count_nodes.py
+++ hw1/count_nodes.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python
 import re,sys
 
+#matches anything that was reached in the BFS
 path = re.compile(r'\s+\d+\|[0-7]\|\d+')
 
 def count(line):
     count = 0
+
+    #if there is a match, increase count
     if path.search(line):
         count += 1
     return count
@@ -14,6 +17,8 @@ def main():
     num_nodes = 0
     for line in lines:
         num_nodes += count(line)
+
+    #print total number of nodes
     print num_nodes
 
 main()

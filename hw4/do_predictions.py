b05f9ac61345528d6b04bc6f95e45a3e979d9d58
Doug von Kohorn <dougvk@gmail.com>
2010-11-29 15:27:43 -0500

    all done

--- hw4/do_predictions.py
+++ hw4/do_predictions.py
@@ -3,11 +3,59 @@ import igraph, numpy, sys, random
 from igraph import *
 from numpy import *
 
-def print_evaluation(attachment,adamic_adar,jaccard,common_neighbor,shortest_distance):
+def print_evaluation(graph,attachment,adamic_adar,jaccard,common_neighbor,shortest_distance):
+    #max number of correct edges
+    denominator = len(attachment)
+
+    num_attach = 0
+    for el in attachment:
+        try:
+            graph.get_eid(el[0][0],el[0][1])
+            num_attach += 1
+        except:
+            pass
+
+    num_adamic = 0
+    for el in adamic_adar:
+        try:
+            graph.get_eid(el[0][0],el[0][1])
+            num_adamic += 1
+        except:
+            pass
+
+    num_jaccard = 0
+    for el in jaccard:
+        try:
+            graph.get_eid(el[0][0],el[0][1])
+            num_jaccard += 1
+        except:
+            pass
+
+    num_common = 0
+    for el in common_neighbor:
+        try:
+            graph.get_eid(el[0][0],el[0][1])
+            num_common += 1
+        except:
+            pass
+
+    num_distance = 0
+    for el in shortest_distance:
+        try:
+            graph.get_eid(el[0][0],el[0][1])
+            num_distance += 1
+        except:
+            pass
+
+    print "Preferential attachment: %s\nAdamic/Adar: %s\nJaccard: %s \
+            \nCommon Neighbor: %s\nDistance: %s" \
+            % (num_attach/float(denominator),num_adamic/float(denominator), \
+            num_jaccard/float(denominator),num_common/float(denominator), \
+            num_distance/float(denominator))
 
 def main():
     #load graph, clean it up
-    graph = Graph.Erdos_Renyi(70,.5)
+    graph = Graph.Read_GraphMLz("wiki.graphmlz")
     graph.to_undirected()
     self_loops = [edge.index for edge in graph.es if edge.source == edge.target]
     graph.delete_edges(self_loops)
@@ -29,23 +77,42 @@ def main():
     nodes = sorted(list(node_set))
 
     #retrieve the subgraph over those nodes
-    subgraph = graph.subgraph(nodes)
+    g_training = graph.subgraph(nodes)
 
     #find n (difference between true # of edges in subgraph
     #and number that we are training from (1000))
-    n = len(subgraph.es) - len(edge_list)
+    n = len(g_training.es) - len(edge_list)
+
+    #remove edges that g_training shouldn't know about
+    to_delete = []
+    full_list = g_training.get_edgelist()
+    for edge in full_list:
+        try:
+            edge_list.index(edge)
+        #delete edge if it doesn't exist in our edge_list
+        except:
+            to_delete.append(edge)
+
+    #get the edge ids of all the edges to delete, and delete them
+    eids = []
+    for edge in to_delete:
+        eids.append(g_training.get_eid(edge[0],edge[1]))
+    g_training.delete_edges(eids)
+
+    #update node ids for g_training
+    nodes = range(len(g_training.vs))
 
     #shortest distance
     scores = []
     #returns length of shortest path from all nodes to all nodes
     #where nodes are the nodes we care about
-    path_lengths = graph.shortest_paths_dijkstra(nodes,mode=ALL)
+    path_lengths = g_training.shortest_paths_dijkstra(nodes,mode=ALL)
     for x in nodes:
         #this loop just makes sure we don't double count
         for y in [node for node in nodes if node > x]:
             #if the edge exists in g_training, don't add it
             try:
-                edge_list.index((x,y))
+                g_training.get_eid(x,y)
             except:
                 #add all paths of length 2
                 if path_lengths[x][y] == 2:
@@ -66,9 +133,9 @@ def main():
     for i in range(len(nodes)):
         for j in range(len(nodes)):
             if i != j:
-                #if edge exists, add to neighbors
+                #if edge exists in g_training, add to neighbors
                 try:
-                    graph.get_eid(i,j)
+                    g_training.get_eid(i,j)
                     neighbors[str(i)].append(j)
                 except:
                     pass
@@ -149,6 +216,6 @@ def main():
     scores = sorted(scores, key=lambda score: score[1], reverse=True)
     attachment = scores[:n]
 
-    print_evaluation(attachment,adamic_adar,jaccard,common_neighbor,shortest_distance)
+    print_evaluation(graph,attachment,adamic_adar,jaccard,common_neighbor,shortest_distance)
 
 main()
